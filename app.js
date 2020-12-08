0a0e572ac347bbfc32c8e4c1203e24671e98715f
Dylan Crawshaw <dcrawsh@gmail.com>
2020-12-08 13:22:48 -0800

    add response

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ const http = require('http');
 
 
 
-const server = http.createServer((res,req) => {
+const server = http.createServer((req,res) => {
     console.log(req);
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<body><h1>Hello from Node.js Server!</h1></body>');
+    res.write('</html>');
+    res.end();
 });
 
 server.listen(3000);
