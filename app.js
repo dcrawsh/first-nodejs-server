8797ff01a1dca0d9b335ec321f3055fbcf78a75c
Dylan Crawshaw <dcrawsh@gmail.com>
2021-01-27 19:56:20 -0800

    export routes

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const http = require('http');
 
+const routes = require('./routes');
 
 
-const server = http.createServer((req,res) => {
-  
-   
-});
+
+const server = http.createServer(routes);
 
 server.listen(3000);
