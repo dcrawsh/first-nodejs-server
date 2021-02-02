a830cc72fef6bef2ac401c439207828204271b3d
Dylan Crawshaw <dcrawsh@gmail.com>
2021-02-01 18:13:48 -0800

    add middleware

--- app.js
+++ app.js
@@ -2,9 +2,17 @@ const http = require('http');
 
 const express = require('express');
 
-const app = express();
+const app = express() ;
 
+app.use((req,res,next) => {
+    console.log('in the middle');
+    next();
+})
+app.use((req,res,next) => {
+    console.log('in a different middle');
+    
+})
 
 const server = http.createServer(app);
 
-server.listen(3000);
+server.listen(3001);
