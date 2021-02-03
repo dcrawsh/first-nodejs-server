a57123d7086b4e4dd47e5ddd9d7e1a45c6d3aec5
Dylan Crawshaw <dcrawsh@gmail.com>
2021-02-02 19:21:00 -0800

    add express router

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 const port = 3001;
 
 app.use(bodyParser.urlencoded({extended: false}));
 
+app.use(shopRoutes);
+app.use(adminRoutes);
 
 
-app.use('/add-product',(req,res,next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</form>');
-});
 
-app.post('/product',(req,res,next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
 
-app.use('/',(req,res,next) => {
-    res.send('<h1>This is the root</h1>');
-})
+
+
+
 
 app.listen(port);
