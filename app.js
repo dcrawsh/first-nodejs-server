f55eba393ccd0f5a27b6bf821c6d1a220dbf5834
Dylan Crawshaw <dcrawsh@gmail.com>
2021-02-02 19:00:04 -0800

    add body-parser middleware

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const port = 3001;
 
-app.get('/other',(req,res,next) => {
-    console.log('other');
-    res.send('<h1>This is the other</h1>');
-})
-app.get('/',(req,res,next) => {
-    console.log(root);
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+
+app.use('/add-product',(req,res,next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</form>');
+});
+
+app.use('/product',(req,res,next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/',(req,res,next) => {
     res.send('<h1>This is the root</h1>');
 })
 
