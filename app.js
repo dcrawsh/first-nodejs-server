c6f355860b6bf7dfbeaea99f3eb641963d41e80c
Dylan Crawshaw <dcrawsh@gmail.com>
2021-02-02 19:25:33 -0800

    add 404 error page

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(shopRoutes);
 app.use(adminRoutes);
 
+app.use((req,res,next) => {
+    res.status(404).send('<h1>Page not found</h1>');
+});
+
 
 
 
