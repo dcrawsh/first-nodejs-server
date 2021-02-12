617423634758c0499d907eec435a9d69b48b96fb
Dylan Crawshaw <dcrawsh@gmail.com>
2021-02-11 16:58:01 -0800

    refactor

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const port = 3001;
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(shopRoutes);
-app.use('/admin',adminRoutes);
+app.use('/admin',adminData.routes);
 
 app.use((req,res,next) => {
     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
